036492516d96b1bb3d0a35ada49f647bfa60c72e
Roy Hsu <roy.hsu@tinyworld.cc>
2016-09-11 23:31:58 +0800

    add test for web service

--- CHFoundation/CHFoundationTests/WebServiceTests.swift
+++ CHFoundation/CHFoundationTests/WebServiceTests.swift
@@ -9,81 +9,73 @@
 @testable import CHFoundation
 import XCTest
 
+class User {
+    
+    let name: String
+    
+    init(name: String) {
+        
+        self.name = name
+        
+    }
+    
+}
+
 class WebServiceTests: XCTestCase {
     
-//    var webService: WebService?
+    var webService: WebService<User>? = nil
+    var mockUserData: Data {
+        
+        return try! JSONSerialization.data(
+            withJSONObject: [ "name": "Roy" ],
+            options: []
+        )
+        
+    }
     
     override func setUp() {
         super.setUp()
         
-//        let url = URL(string: "https://example.com")!
-//        webService = WebService(urlRequest: URLRequest(url: url))
+        let url = URL(string: "https://example.com")!
+        let urlRequest = URLRequest(url: url)
+        webService = WebService(urlRequest: urlRequest) { data in
+            
+            let json = data as! NSDictionary
+            let name = json["name"] as! String
+            let user = User(name: name)
+            
+            return user
+            
+        }
         
     }
     
     override func tearDown() {
     
-//        webService = nil
+        webService = nil
         
         super.tearDown()
     }
     
-//    func testSuccessfulResponse() {
-//        
-//        class MockURLSession: URLSession {
-//            
-//            override func dataTask(with url: URL, completionHandler: (Data?, URLResponse?, NSError?) -> Void) -> URLSessionDataTask {
-//                
-//                let dataObject: [NSObject: AnyObject] = [
-//                    "data": [
-//                        [ "id": "001", "name": "Allen" ],
-//                        [ "id": "002", "name": "Bob" ],
-//                        [ "id": "003", "name": "Chris" ],
-//                        [ "id": "004", "name": "David" ],
-//                        [ "id": "005", "name": "Emily" ]
-//                    ]
-//                ]
-//                
-//                let jsonData = try! JSONSerialization.data(withJSONObject: dataObject, options: .prettyPrinted)
-//                
-//                let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: "1.1", headerFields: nil)
-//                
-//                completionHandler(jsonData, response, nil)
-//                
-//                return URLSessionDataTask()
-//                
-//            }
-//            
-//        }
-//        
-//        let expectation = self.expectation(withDescription: "Request data with web service.")
-//        
-//        webService!.request(
-//            with: MockURLSession(),
-//            successHandler: { json in
-//                
-//                let jsonObject = json as? [NSObject: AnyObject]
-//                
-//                XCTAssertNotNil(jsonObject, "Should get json object.")
-//                
-//                let bobName = jsonObject!["data"]?[2]?["name"]
-//                
-//                XCTAssertEqual(bobName, "Bob", "The result doesn't match.")
-//                
-//                expectation.fulfill()
-//                
-//            },
-//            failHandler: { error in
-//            
-//                XCTAssertNil(error, "Should not catch an error.")
-//                
-//                expectation.fulfill()
-//                
-//            }
-//        )
-//        
-//        waitForExpectations(withTimeout: 3.0, handler: nil)
-//        
-//    }
+    func testSuccessfulResponse() {
+    
+        let expectation = self.expectation(description: "Request data with web service.")
+        let mockSession = MockURLSession()
+        mockSession.data = mockUserData
+        
+        let _ = webService!
+            .request(with: mockSession)
+            .then { user -> Void in
+                
+                let expectedUser = User(name: "Roy")
+                
+                XCTAssertEqual(user.name, expectedUser.name)
+                
+            }
+            .always { expectation.fulfill() }
+        
+        waitForExpectations(timeout: 3.0, handler: nil)
+        
+    }
     
 }
--- Source/WebService.swift
+++ Source/WebService.swift
@@ -6,116 +6,55 @@
 //  Copyright © 2016年 Tiny World. All rights reserved.
 //
 
-import Foundation
+import PromiseKit
 
-public enum WebServiceError: Error {
-    case invalidResponse
-    case message(String)
-    case data(Data?)
-    case modelParsing
-}
+
+// MARK: - WebService
 
 public struct WebService<Model>: Equatable {
     
-    public typealias ErrorParser = (_ data: Data?, _ urlResponse: URLResponse?, _ error: Error?) -> Error
-    public typealias SuccessHandler = (Model) -> Void
-    public typealias FailHandler = (_ statusCode: Int?, _ error: Error) -> Void
+    public typealias ModelParser = (Any) throws -> Model
     
     
     // MARK: Property
-
-    public let webResource: WebResource<Model>
+    
+    public let urlRequest: URLRequest
+    public let modelParser: ModelParser
     
     
     // MARK: Init
     
-    public init(webResource: WebResource<Model>) { self.webResource = webResource }
-    
-    
-    // MARK: Request
-    
-    // TODO:
-    // 1. Option for go back to main queue automatically.
-    // 2. Option for errorParser.
-    
-    public func request(with urlSession: URLSession, errorParser: ErrorParser? = nil, successHandler: @escaping SuccessHandler, failHandler: FailHandler? = nil) -> URLSessionTask {
+    public init(urlRequest: URLRequest, modelParser: @escaping ModelParser) {
         
-        let sessionTask = urlSession.dataTask(with: webResource.urlRequest) { data, response, error in
-            
-            guard let response = response as? HTTPURLResponse else {
-                
-                failHandler?(nil, WebServiceError.invalidResponse)
-                
-                return
-                
-            }
-            
-            let statusCode = response.statusCode
-            
-            if let errorParsingError = errorParser?(data, response, error) {
-                
-                failHandler?(statusCode, errorParsingError)
-                
-                return
-                
-            }
-            
-            if let error = error {
-                
-                let serverError: WebServiceError = .message(error.localizedDescription)
-                
-                failHandler?(statusCode, serverError)
-                
-                return
-            
-            }
-            
-            if !self.validate(withStatusCode: statusCode) {
-             
-                let serverError: WebServiceError = .message(NSLocalizedString("Unaccepted status code.", comment: ""))
-                
-                failHandler?(statusCode, serverError)
-                
-                return
-                
-            }
-            
-            do {
-                
-                let data = data ?? Data()
-                let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
-                
-                guard let model = self.webResource.parse(jsonObject) else {
-                    
-                    let error: WebServiceError = .modelParsing
-                    
-                    failHandler?(nil, error)
-                    
-                    return
-                    
-                }
-                
-                successHandler(model)
-                
-            }
-            catch { failHandler?(statusCode, error) }
-        
-        }
-        
-        sessionTask.resume()
-        
-        return sessionTask
+        self.urlRequest = urlRequest
+        self.modelParser = modelParser
         
     }
     
     
-    // MARK: Validate
+    // MARK: Request
     
-    private func validate(withStatusCode statusCode: Int) -> Bool {
+    public func request(with urlSession: URLSession) -> Promise<Model> {
         
-        switch statusCode {
-        case 200..<300: return true
-        default: return false
+        return Promise { fulfill, reject in
+            
+            let _ = urlSession
+                .dataTask(with: self.urlRequest)
+                .then { data -> Void in
+                    
+                    do {
+                        
+                        let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                        let model = try self.modelParser(jsonObject)
+                        
+                        fulfill(model)
+                        
+                    }
+                    catch { reject(error) }
+                    
+                }
+                .catch { error in reject(error) }
+            
         }
         
     }
@@ -123,10 +62,10 @@ public struct WebService<Model>: Equatable {
 }
 
 
-// MARK: Equatable
+// MARK: - Equatable
 
 public func ==<Model>(lhs: WebService<Model>, rhs: WebService<Model>) -> Bool {
     
-    return lhs.webResource == rhs.webResource
+    return lhs.urlRequest == rhs.urlRequest
     
 }
