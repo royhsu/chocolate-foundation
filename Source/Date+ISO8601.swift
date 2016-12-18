779b0d3bb2223564e5ff0fa276e461a72f8c2a64
許郁棋 <roy@Roys-MacBook-12.local>
2016-12-18 10:06:27 +0800

    add reference

--- Source/Date+ISO8601.swift
+++ Source/Date+ISO8601.swift
@@ -4,6 +4,7 @@
 //
 //  Created by 許郁棋 on 2016/12/18.
 //  Copyright © 2016年 Tiny World. All rights reserved.
+//  Reference: http://stackoverflow.com/questions/28016578/swift-how-to-create-a-date-time-stamp-and-format-as-iso-8601-rfc-3339-utc-tim
 //
 
 import Foundation
